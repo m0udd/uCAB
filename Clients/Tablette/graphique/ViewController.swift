550ec3b3b6ea4e8f707090d27908bf00beceba4c
Rémi LAVIELLE <remi.lavielle@imerir.com>
2015-10-22 08:34:52 +0200

    Client iOS Debut graphique

--- Clients/Tablette/graphique/ViewController.swift
+++ Clients/Tablette/graphique/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     let monClient = Client()
     
     @IBAction func Connexion(sender: AnyObject) {
+        monClient.dialogueServeur()
     }
     
     // A chaque clic de la souris sur la surface aréa
